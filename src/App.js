1f69cdaf4b8c826250a554439a6792e80dfb2d6a
nighteleve <nighteleve9@gmail.com>
2020-05-12 16:16:30 +0900

    #1.3 props와 state를 사용한 간단한 Todo 애플리케이션

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import HelloMessage from "./HelloMessage";
 import Timer from "./Timer";
+import TodoApp from "./TodoApp";
 export default function App() {
   return (
     <div className="App">
       <HelloMessage name="Taylor" />
       <Timer />
+      <TodoApp />
     </div>
   );
 }
