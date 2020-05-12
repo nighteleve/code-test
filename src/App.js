6e9602048c1f4903a6725e0a20d4a50a2a9fd0f5
nighteleve <nighteleve9@gmail.com>
2020-05-12 16:11:00 +0900

    #1.2 Timer 상태를 가지는 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import HelloMessage from "./HelloMessage";
+import Timer from "./Timer";
 export default function App() {
   return (
     <div className="App">
       <HelloMessage name="Taylor" />
+      <Timer />
     </div>
   );
 }
